a1a0fb3ece81d81da60d4c35a5037f464a508cfe
dbordson <bozzod@gmail.com>
2014-02-23 21:05:49 -0600

    Oops, this time with the xmlmanager.py changes add to the commit.

--- xmlmanager.py
+++ xmlmanager.py
@@ -1,13 +1,14 @@
 def filemapper():
 	import os
 	xmldirectory = []
-	i = 0
 	for root, dirs, files in os.walk ("/Users/David/882095"):
 		for file in files:
 			if file.endswith('.xml'):
-				i = i + 1
+#				if os.path.join(root, file).find("0001179110") == -1:
+#					print os.path.join(root, file)
 				xmldirectory.append(os.path.join(root, file))
 	return xmldirectory
 
 print "How long is the returned directory list?"
 print(len(filemapper()))
+
