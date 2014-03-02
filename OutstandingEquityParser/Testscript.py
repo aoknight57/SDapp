45d4d25ed4f1fdb1a31201a38a04a82acf1fd713
dbordson <bozzod@gmail.com>
2014-03-01 21:52:38 -0600

    Cleaning up ancillary files

--- OutstandingEquityParser/Testscript.py
+++ OutstandingEquityParser/Testscript.py
@@ -3,6 +3,8 @@
 # 2. Figure out how to handle the circumstances where the header is garbled
 # 	 and the columns are out of order -- need to detect this error and try to
 #	 fix it (start with evaluating cell formats for clues)
+# 3. Rewrite FTP script to pull in the quarterly indices (ftp://ftp.sec.gov/edgar/full-index/2014/QTR1/)
+# note this regarding the html parser: https://github.com/pydata/pandas/issues/4683
 
 
 import os
