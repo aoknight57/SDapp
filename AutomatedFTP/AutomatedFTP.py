7798efae12a739268edb7dfd54b442941e142f8f
dbordson <bozzod@gmail.com>
2014-03-06 00:14:00 -0600

    Added update mode for FTP, but significant refactoring to be done

--- AutomatedFTP/AutomatedFTP.py
+++ AutomatedFTP/AutomatedFTP.py
@@ -79,27 +79,44 @@ if indexdownload == 'y':
 if indexdownload == "n":
 	print "On to the next question"
 print " "
-indexupdate = inputq("Would you like to replace the most recent source index and look for a newer one? (don't do this until you have downloaded all past source indices)", ['y', 'n'])
+indexupdate = inputq("Would you like to replace the most recent quarterly source index and look for a newer one? (don't do this until you have downloaded all past source indices)", ['y', 'n'])
 if indexupdate == 'y':
 	print "Okay, we won't download this file just yet, but it will be a part of the ftp package"
 if indexupdate == "n":
 	print "On to the next question"
+print " "
+initializing = inputq("Would you like to do an initializing download?", ['y', 'n'])
+if initializing == 'y':
+	print "Okay, we will do an initializing download"
+	update = 'n'
+if initializing == "n":
+	print "On to the next question" 
+	update = inputq("Would you like to do an update download?", ['y', 'n'])
+	if update == 'y':
+		print "Okay, we will do an update download"
+		print "What is the date of the update? (YYYYMMDD)"
+		date = raw_input()
+	if update == "n":
+		print "On to the next question"
 
 
-indexfilelist = []
-indexdirectory = cwd + '/FilingIndices/'
 
-if not(os.path.exists(indexdirectory)):
-	os.makedirs(indexdirectory)
-	print "Created a directory: ", indexdirectory
+qindexfilelist = []
+qindexdirectory = cwd + '/QFilingIndices/'
 
-for filename in os.listdir(indexdirectory):
+if not(os.path.exists(qindexdirectory)):
+	os.makedirs(qindexdirectory)
+	print "Created a directory: ", qindexdirectory
+
+for filename in os.listdir(qindexdirectory):
 	if filename.endswith('.txt'):
-		indexfilelist.append(indexdirectory + filename)
+		qindexfilelist.append(qindexdirectory + filename)
 
+dindexdirectory = cwd + '/DFilingIndices/'
+if not(os.path.exists(dindexdirectory)):
+	os.makedirs(dindexdirectory)
+	print "Created a directory: ", dindexdirectory
 
-print "We will work with the CIKs in the stored CIK file" 
-print ''
 
 
 
@@ -114,36 +131,63 @@ formdir = form
 if form == '4/A':
 	formdir = '4A'
 
-CIKs = []
-# This only runs if there is no CIKs.txt file; if none exists,
-# it creates this file and populates it with '882095'
-if not(os.path.isfile('CIKs.txt')):
-	target = open(cwd + '/' + 'CIKs.txt', 'w')
-	print>>target, '882095'
-	target.close()
-
-with open("CIKs.txt") as infile:
-	for line in infile:
-		CIKs.append(line.strip())
-print "Using CIKs:", CIKs
-print " "
 
 
-# Makes the storage directories if they are missing/not yet created
-for CIK in CIKs:
-	directory = "storage/" + str(CIK) + '/' + str(formdir)
-	if not(os.path.exists(directory)):
-		os.makedirs(directory)
-		print "Created a directory: ", directory
+if initializing == 'y':
+	CIKsInit = []
+	# This only runs if there is no CIKsInit.txt file; if none exists,
+	# it creates this file and populates it with '882095'
+	if not(os.path.isfile('CIKsInit.txt')):
+		target = open(cwd + '/' + 'CIKsInit.txt', 'w')
+		print>>target, '882095'
+		target.close()
+
+	with open("CIKsInit.txt") as infile:
+		for line in infile:
+			CIKsInit.append(line.strip())
+	print "Using initialization CIKs:", CIKsInit
+	print " "
+	# Makes the storage directories if they are missing/not yet created
+	for CIK in CIKsInit:
+		directory = "storage/" + str(CIK) + '/' + str(formdir)
+		if not(os.path.exists(directory)):
+			os.makedirs(directory)
+			print "Created a directory: ", directory
 
 
-print "Okay, lets download the files we need from the SEC site."
-print "This script will rebuild its index of files for each CIK."
-print "If any source forms on the new index have not yet been downloaded,"
-print "the script will attempt to download those forms."
-print "The first several times you run this script, it will be fast, but"
-print "Over time it will slow down substantially, because the indices will"
-print "become larger and related files will become more numerous"
+
+
+if update == 'y':
+	CIKsUpdate = []
+	# This only runs if there is no CIKsUpdate.txt file; if none exists,
+	# it creates this file and populates it with '882095'
+	if not(os.path.isfile('CIKsUpdate.txt')):
+		target = open(cwd + '/' + 'CIKsUpdate.txt', 'w')
+		print>>target, '882095'
+		target.close()
+
+	with open("CIKsUpdate.txt") as infile:
+		for line in infile:
+			CIKsUpdate.append(line.strip())
+	print "Using update CIKs:", CIKsUpdate
+	print " "
+	# Makes the storage directories if they are missing/not yet created
+	for CIK in CIKsUpdate:
+		directory = "storage/" + str(CIK) + '/' + str(formdir)
+		if not(os.path.exists(directory)):
+			os.makedirs(directory)
+			print "Created a directory: ", directory
+
+
+
+
+print "Okay, let's download the files we need from the SEC site."
+#print "This script will rebuild its index of files for each CIK."
+#print "If any source forms on the new index have not yet been downloaded,"
+#print "the script will attempt to download those forms."
+#print "The first several times you run this script, it will be fast, but"
+#print "Over time it will slow down substantially, because the indices will"
+#print "become larger and related files will become more numerous"
 
 runftp = inputq("Would you like to continue?", ['y', 'n'])
 if runftp == 'y':
@@ -153,7 +197,7 @@ if runftp == "n":
 	exit(0)
 
 
-if indexupdate == 'y' or indexdownload == 'y':
+if runftp == 'y':
 	print "Logging on to the SEC site for index download"
 	try:
 		target = open(cwd + '/' + 'emailaddress.txt')
@@ -167,10 +211,10 @@ if indexupdate == 'y' or indexdownload == 'y':
 		print "Cannot connect, quitting"
 		exit(0)
 
-indexbasepath = "/edgar/full-index"
+qindexbasepath = "/edgar/full-index"
 
 if indexupdate == 'y':
-	mostrecentindex = max(indexfilelist)
+	mostrecentindex = max(qindexfilelist)
 	
 	templength = len(mostrecentindex)
 	mostrecentindex = mostrecentindex[templength - 10: templength]
@@ -178,8 +222,8 @@ if indexupdate == 'y':
 	latestyear = mostrecentindex[:4]
 	latestquarter = 'QTR' + mostrecentindex[5]
 	try:
-		ftp.cwd(indexbasepath + "/" + latestyear + "/" + latestquarter)
-		local_filename = os.getcwd() + "/FilingIndices/" + latestyear + "Q" + latestquarter[3] + ".txt"
+		ftp.cwd(qindexbasepath + "/" + latestyear + "/" + latestquarter)
+		local_filename = os.getcwd() + "/QFilingIndices/" + latestyear + "Q" + latestquarter[3] + ".txt"
 		target = open(local_filename, 'wb')
 		ftp.retrbinary('RETR %s' % "form.idx", target.write) 
 		target.close
@@ -195,8 +239,8 @@ if indexupdate == 'y':
 		checkquarter = 'QTR' + str(int(latestquarter[3]) + 1)
 	print checkyear, checkquarter
 	try:
-		ftp.cwd(indexbasepath + "/" + checkyear + "/" + checkquarter)
-		local_filename = os.getcwd() + "/FilingIndices/" + checkyear + "Q" + checkquarter[3] + ".txt"
+		ftp.cwd(qindexbasepath + "/" + checkyear + "/" + checkquarter)
+		local_filename = os.getcwd() + "/QFilingIndices/" + checkyear + "Q" + checkquarter[3] + ".txt"
 		target = open(local_filename, 'wb')
 		ftp.retrbinary('RETR %s' % "form.idx", target.write) 
 		target.close
@@ -210,7 +254,7 @@ if indexupdate == 'y':
 download = 0
 if indexdownload == 'y':
 	
-	ftp.cwd(indexbasepath)
+	ftp.cwd(qindexbasepath)
 	print "now in /edgar/full-index directory" 
 	indexyearlist = []
 	ftp.retrlines('nlst', indexyearlist.append)
@@ -221,15 +265,15 @@ if indexdownload == 'y':
 			templist.append(entry)
 	indexyearlist = templist
 	print "Now going to go one level deeper and begin to save any missing indices..."
-	fullindexdirectory = []
+	fullqindexdirectory = []
 	downloadcounter = 1
 	for year in indexyearlist:
 		quarterlist = []
 		# print "building subdirectory ", year
 		
-		ftp.cwd(indexbasepath + "/" + year)
+		ftp.cwd(qindexbasepath + "/" + year)
 		ftp.retrlines('nlst', quarterlist.append)
-		fullindexdirectory.append(quarterlist)
+		fullqindexdirectory.append(quarterlist)
 
 
 		templist = []
@@ -241,15 +285,15 @@ if indexdownload == 'y':
 		
 		for quarter in quarterlist:
 			
-			if not any(existingindex.find(year + 'Q' + quarter[3] + '.txt') != -1 for existingindex in indexfilelist):
+			if not any(existingindex.find(year + 'Q' + quarter[3] + '.txt') != -1 for existingindex in qindexfilelist):
 				print '\t' + str(downloadcounter) + ". Trying to download:", year + 'Q' + quarter[3], "..."				
 				downloadcounter += 1
 				try:
-					ftp.cwd(indexbasepath + "/" + year + "/" + quarter)
-					local_filename = os.getcwd() + "/FilingIndices/" + year + "Q" + quarter[3] + ".txt"
+					ftp.cwd(qindexbasepath + "/" + year + "/" + quarter)
+					local_filename = os.getcwd() + "/QFilingIndices/" + year + "Q" + quarter[3] + ".txt"
 					target = open(local_filename, 'wb')
 					ftp.retrbinary('RETR %s' % "form.idx", target.write)
-					target.close
+					target.close()
 					download += 1
 					print '\t\tdone'
 				except: 
@@ -261,100 +305,216 @@ if indexdownload == 'y':
 			break
 	print "Done with index file download attempt"
 
-if indexupdate == 'y' or indexdownload == 'y':
-	ftp.close()	
+dindexbasepath = "/edgar/daily-index"
+if update == 'y':
+	print "Downloading daily index..."
+	try:
+		ftp.cwd(dindexbasepath)
+		local_filename = os.getcwd() + "/DFilingIndices/" + date + ".txt"
+		target = open(local_filename, 'wb')
+		ftp.retrbinary('RETR %s' % ("form." + date + ".idx"), target.write)
+		target.close()
+		
+		print '\t\tdone'
+	except: 
+		print "Can't get file in ", date
 
-
-
-indexfilelist = []
-for filename in os.listdir(indexdirectory):
+ftp.close()
+qindexfilelist = []
+for filename in os.listdir(qindexdirectory):
 	if filename.endswith('.txt'):
-		indexfilelist.append(indexdirectory + filename)
+		qindexfilelist.append(qindexdirectory + filename)
 
 
 print "Now lets generate the list of forms we need from the indices we have."
 
 
-for CIK in CIKs:
-	print "\t Working on:", CIK
-	target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt', 'w')	
-#	formfilelist = []
-	
-	i = 0
-	for index in indexfilelist:
+#	print "\t Working on:", CIK
+
+if initializing == 'y':
+	for CIK in CIKsInit:
+		target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt', 'w')	
+		target.close()
+	#	formfilelist = []
+		
+		i = 0
+	LastCIK = '911911911911911911911911911'
+	for index in qindexfilelist:
+
 		with open(index) as infile:
 			for line in infile:
-				if line.find(' ' + str(CIK) + ' ') != -1 and line.find(str(form)+'  ') == 0:
+				if line.find(' ' + str(LastCIK) + ' ') != -1 and line.find(str(form)+'  ') == 0:
+					
 					formfilename = line[line.find("edgar/data/"):len(line)]
 					formfilename = formfilename.rstrip()
-					
 					print>>target, formfilename
+
+				elif line.find(str(form)+'  ') == 0:
+					for CIK in CIKsInit:
+						if line.find(' ' + str(CIK) + ' ') != -1:
+							target.close()
+							target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt', 'a')
+							LastCIK = CIK
+							formfilename = line[line.find("edgar/data/"):len(line)]
+							formfilename = formfilename.rstrip()
+							print>>target, formfilename
+	#					formfilelist.append(formfilename)
+		
+		target.close()
+		LastCIK = '911911911911911911911911911'
+
+if update == 'y':
+	for CIK in CIKsUpdate:
+		target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + 'update.txt', 'w')	
+		target.close()
+	#	formfilelist = []
+		
+		i = 0
+	LastCIK = '911911911911911911911911911'
+	with open(os.getcwd() + "/DFilingIndices/" + date + ".txt") as infile:
+		for line in infile:
+			if line.find(' ' + str(LastCIK) + ' ') != -1 and line.find(str(form)+'  ') == 0:
+				
+				formfilename = line[line.find("edgar/data/"):len(line)]
+				formfilename = formfilename.rstrip()
+				print>>target, formfilename
+
+			elif line.find(str(form)+'  ') == 0:
+				for CIK in CIKsUpdate:
+					if line.find(' ' + str(CIK) + ' ') != -1:
+						target.close()
+						target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + 'update.txt', 'a')
+						LastCIK = CIK
+						formfilename = line[line.find("edgar/data/"):len(line)]
+						formfilename = formfilename.rstrip()
+						print>>target, formfilename
 #					formfilelist.append(formfilename)
 	
 	target.close()
-
-print "Logging in to the SEC site to download source files"
-# The purpose of logging in and out twice is because it seems like the SEC ftp 
-# site stops responding wait too long in between requests.  There is a 
-# significant amount of processing time involved in getting from the raw index 
-# lists to indices for each CIK
-try:
-	target = open(cwd + '/' + 'emailaddress.txt')
-	email = target.read()
-	email = email.strip()
-	target.close()
-	ftp = FTP('ftp.sec.gov') 
-	ftp.login('anonymous', email)
-	print "Connected"
-except:
-	print "Cannot connect, quitting"
-	exit(0)
-
-for CIK in CIKs:
-	ftpbasedirectory = '/edgar/data/' + str(CIK)
-
-	
-
-	existingfilestring = os.listdir(cwd + '/' + "storage/" + str(CIK) + '/' + str(formdir) + '/')
-	
-	tempfilestring = []
-
-	for item in existingfilestring:
-		if item.endswith('.xml') or item.endswith('.txt'):
-			reformatlocation = item.find('-')
-			tempfilestring.append(item[:reformatlocation] + '/' + item[reformatlocation + 1:len(item)-4])
-	existingfilestring = tempfilestring
-	
-
-	
-	#print ftpbasedirectory
-	#ftp.cwd(ftpbasedirectory)
-
-	with open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt') as sourcefile:
-		print "CIK loop", CIK
+	LastCIK = '911911911911911911911911911'
+	for CIK in CIKsUpdate:
+		with open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + 'update.txt') as sourcefile:
+			target = open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt', 'a+')
+			targetstring = target.read()
+			for line in sourcefile:
+				if targetstring.find(line.strip()) == -1:
+					print>>target, line.strip()
+			target.close()
 		
-		for line in sourcefile:
+if initializing == 'y' or update == 'y':
+	print "Logging in to the SEC site to download source files"
+	# The purpose of logging in and out twice is because it seems like the SEC ftp 
+	# site stops responding wait too long in between requests.  There is a 
+	# significant amount of processing time involved in getting from the raw index 
+	# lists to indices for each CIK
+	try:
+		target = open(cwd + '/' + 'emailaddress.txt')
+		email = target.read()
+		email = email.strip()
+		target.close()
+		ftp = FTP('ftp.sec.gov') 
+		ftp.login('anonymous', email)
+		print "Connected"
+	except:
+		print "Cannot connect, quitting"
+		exit(0)
 
+if initializing == 'y':
+
+	for CIK in CIKsInit:
+		ftpbasedirectory = '/edgar/data/' + str(CIK)
+
+		
+
+		existingfilestring = os.listdir(cwd + '/' + "storage/" + str(CIK) + '/' + str(formdir) + '/')
+		
+		tempfilestring = []
+
+		for item in existingfilestring:
+			if item.endswith('.xml') or item.endswith('.txt'):
+				reformatlocation = item.find('-')
+				tempfilestring.append(item[:reformatlocation] + '/' + item[reformatlocation + 1:len(item)-4])
+		existingfilestring = tempfilestring
+		
+
+		
+		#print ftpbasedirectory
+		#ftp.cwd(ftpbasedirectory)
+
+		with open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + '.txt') as sourcefile:
+			print "CIK loop", CIK
 			
-			if not any(line.find(existingfile) != -1 for existingfile in existingfilestring):
+			for line in sourcefile:
+
 				
-				try:
+				if not any(line.find(existingfile) != -1 for existingfile in existingfilestring):
 					
-					namestart = line.find('/', len("edgar/data/"))
-					filerCIK = line[len("edgar/data/"):namestart]
-					#print line[0:namestart]
-					#workingdirectory = ('/' + line[0:namestart]).strip()
-					#ftp.cwd(workingdirectory)
-					filepath = line.strip()
-					endline = filepath.find('.txt')
-					filename = filepath[namestart + 1:endline+1]
-					local_filename = os.getcwd() + "/" + "storage/" + str(CIK) + '/' + str(formdir) + '/' + filerCIK + '-' + filename + "txt"
-					target = open(local_filename, 'wb')
+					try:
+						
+						namestart = line.find('/', len("edgar/data/"))
+						filerCIK = line[len("edgar/data/"):namestart]
+						#print line[0:namestart]
+						#workingdirectory = ('/' + line[0:namestart]).strip()
+						#ftp.cwd(workingdirectory)
+						filepath = line.strip()
+						endline = filepath.find('.txt')
+						filename = filepath[namestart + 1:endline+1]
+						local_filename = os.getcwd() + "/" + "storage/" + str(CIK) + '/' + str(formdir) + '/' + filerCIK + '-' + filename + "txt"
+						target = open(local_filename, 'wb')
+						
+						ftp.retrbinary('RETR ' + filepath, target.write)
+						target.close()
+					except: 
+						print "Can't get file in:", line
+
+
+if update == 'y':
+
+	for CIK in CIKsUpdate:
+		ftpbasedirectory = '/edgar/data/' + str(CIK)
+
+		
+
+		existingfilestring = os.listdir(cwd + '/' + "storage/" + str(CIK) + '/' + str(formdir) + '/')
+		
+		tempfilestring = []
+
+		for item in existingfilestring:
+			if item.endswith('.xml') or item.endswith('.txt'):
+				reformatlocation = item.find('-')
+				tempfilestring.append(item[:reformatlocation] + '/' + item[reformatlocation + 1:len(item)-4])
+		existingfilestring = tempfilestring
+		
+
+		
+		#print ftpbasedirectory
+		#ftp.cwd(ftpbasedirectory)
+
+		with open(cwd + '/' + "storage/" + str(CIK) + '/' + str(CIK) + 'form' + str(formdir) + 'update.txt') as sourcefile:
+			print "CIK loop", CIK
+			
+			for line in sourcefile:
+
+				
+				if not any(line.find(existingfile) != -1 for existingfile in existingfilestring):
 					
-					ftp.retrbinary('RETR ' + filepath, target.write)
-					target.close()
-				except: 
-					print "Can't get file in:", line
+					try:
+						
+						namestart = line.find('/', len("edgar/data/"))
+						filerCIK = line[len("edgar/data/"):namestart]
+						#print line[0:namestart]
+						#workingdirectory = ('/' + line[0:namestart]).strip()
+						#ftp.cwd(workingdirectory)
+						filepath = line.strip()
+						endline = filepath.find('.txt')
+						filename = filepath[namestart + 1:endline+1]
+						local_filename = os.getcwd() + "/" + "storage/" + str(CIK) + '/' + str(formdir) + '/' + filerCIK + '-' + filename + "txt"
+						target = open(local_filename, 'wb')
+						
+						ftp.retrbinary('RETR ' + filepath, target.write)
+						target.close()
+					except: 
+						print "Can't get file in:", line
 
 print "done"
 
@@ -383,7 +543,11 @@ print "done"
 
 ftp.close()
 print "FTP connection closed"
-txttoxml.processxml(cwd, formdir, CIKs)
-print "Saved files converted from .txt to .xml"
+if initializing == 'y':
+	txttoxml.processxml(cwd, formdir, CIKsInit)
+	print "Saved files converted from .txt to .xml"
+if update == 'y':
+	txttoxml.processxml(cwd, formdir, CIKsUpdate)
+	print "Saved files converted from .txt to .xml"
 
 
